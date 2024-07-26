feea781b1e089b8daa9887349d4570da36dc97f3
Andres Lajara <juanlajara001@gmail.com>
2024-07-26 11:06:05 -0500

    added form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = () => {
+	return (
+		<form>
+			<div className="mb-3">
+				<label htmlFor="" className="form-lable"></label>
+				<input type="text" className="form-control" />
+			</div>
+		</form>
+	);
+};
+
+export default Form;
