161cdbfb577322f3e12faed6aa282b492cd65c8c
Andres Lajara <juanlajara001@gmail.com>
2024-08-02 17:10:34 -0500

    + algolia search and hits

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import Form from "./components/Form";
+import Hit from "./components/Hits";
 import { useState } from "react";
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch, SearchBox, Hits } from "react-instantsearch";
 
 function App() {
 	const [alertVisible, setAlertVisibility] = useState(false);
+	const searchClient = algoliasearch(
+		"AJRNF83HF1",
+		"b026d2a27d609c5f767fb59a5257507a"
+	);
 	return (
 		<div>
-			<Form />
+			<InstantSearch
+				searchClient={searchClient}
+				indexName="onboarding_bestbuy_index"
+			>
+				<SearchBox />
+				<Hits hitComponent={Hit} />
+			</InstantSearch>
 		</div>
 	);
 }
